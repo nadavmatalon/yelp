5d3241a8cd3ba4a77b836d8b4be09ef54b1f91a7
Nadav <nadav.matalon@gmail.com>
2014-07-06 18:44:12 +0100

    added contact page and refactored static pages feature tests

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,14 @@
 class StaticPagesController < ApplicationController
-  def home
-  end
 
-  def help
-  end
+	def home
+	end
 
-  def about
-  end
+	def about
+	end
+
+	def contact
+	end
+
+	def help
+	end
 end
--- spec/features/static_pages_feature_spec.rb
+++ spec/features/static_pages_feature_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages >" do
 
+    let(:base_title) {"Yelp | "}
+
 	describe "Home page: " do
 
 		it "should have the content \'Home\'" do
@@ -11,25 +13,11 @@ describe "Static pages >" do
 
         it "should have the title \'Home\'" do
             visit "/static_pages/home"
-            expect(page).to have_title("Yelp | Home")
+            expect(page).to have_title("#{base_title}Home")
         end
 
 	end
 	
-	describe "Help page" do
-
-    	it "should have the content \'Help\'" do
-    		visit "/static_pages/help"
-    		expect(page).to have_content("Help")
-    	end
-
-            it "should have the title \'Yelp | Help\'" do
-            visit "/static_pages/help"
-            expect(page).to have_title("Yelp | Help")
-        end
-
-    end
-
 	describe "About page" do
 
     	it "should have the content \'About Us\'" do
@@ -39,7 +27,33 @@ describe "Static pages >" do
 
             it "should have the title \'Yelp | About Us\'" do
             visit "/static_pages/about"
-            expect(page).to have_title("Yelp | About Us")
+            expect(page).to have_title("#{base_title}About Us")
+        end
+    end
+
+   describe "Contact page" do
+
+        it "should have the content \'Contact Us\'" do
+            visit "/static_pages/contact"
+            expect(page).to have_content("Contact Us")
+        end
+
+            it "should have the title \'Yelp | Contact Us\'" do
+            visit "/static_pages/contact"
+            expect(page).to have_title("#{base_title}Contact Us")
+        end
+    end
+
+    describe "Help page" do
+
+        it "should have the content \'Help\'" do
+            visit "/static_pages/help"
+            expect(page).to have_content("Help")
+        end
+
+            it "should have the title \'Yelp | Help\'" do
+            visit "/static_pages/help"
+            expect(page).to have_title("#{base_title}Help")
         end
     end
 end
