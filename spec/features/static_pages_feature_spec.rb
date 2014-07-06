d7e374d956c0cae05d127140a5b297137e3a40c3
Nadav <nadav.matalon@gmail.com>
2014-07-06 15:46:24 +0100

    added an about us page

--- spec/features/static_pages_feature_spec.rb
+++ spec/features/static_pages_feature_spec.rb
@@ -13,9 +13,19 @@ describe "Static pages >" do
 	
 	describe "Help page" do
 
-    	it "should have the content \'help\'" do
-    		visit '/static_pages/help'
+    	it "should have the content \'Help\'" do
+    		visit "/static_pages/help"
     		expect(page).to have_content("Help")
     	end
     end
+
+	describe "About page" do
+
+    	it "should have the content \'About Us\'" do
+    		visit "/static_pages/about"
+    		expect(page).to have_content("About Us")
+    	end
+    end
+
 end
+
