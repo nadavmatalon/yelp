86d7857a77f2f088e6308efee15fe6f3352cf4e2
Nadav <nadav.matalon@gmail.com>
2014-07-09 00:08:28 +0100

    added user validations and tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe User do
 
-  before { @user = User.new(name: "John Apple", email: "ja@gmail.com") }
+  before (:each) { @user = User.new(name: "John Apple", email: "ja@gmail.com") }
 
   subject { @user }
 
@@ -15,4 +15,58 @@ describe User do
  		expect(@user).to respond_to(:email)
  		expect(@user.email).to eq "ja@gmail.com"
  	end
+
+
+	describe "name" do
+
+ 		it "cannot be blank" do
+ 			@user.name = "a" * 51
+  			expect(@user).not_to be_valid
+  		end
+  	
+		it "cannot be more than 50 chars" do
+ 			@user.name = " "
+ 			expect(@user).not_to be_valid
+  		end
+	end
+
+	describe "email" do
+
+ 		it "cannot be blank" do
+ 			@user.email = " "
+ 			expect(@user).not_to be_valid
+  		end
+
+    	it "should have valid format" do
+      		addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                        foo@bar_baz.com foo@bar+baz.com]
+      		addresses.each do |invalid_address|
+        		@user.email = invalid_address
+        		expect(@user).not_to be_valid
+      		end
+    	end
+ 
+    	it "must have valid format to create user" do
+      		addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      		addresses.each do |valid_address|
+        		@user.email = valid_address
+        		expect(@user).to be_valid
+      		end
+    	end
+
+ 		it "must be unique" do
+	      	user_with_same_email = @user.dup
+	      	@user.save
+      		user_with_same_email.save
+      		 expect(user_with_same_email).not_to be_valid
+    	end
+
+    	it "must is not case sensitive" do
+	      	user_with_same_email = @user.dup
+	      	user_with_same_email.email = @user.email.upcase
+	      	@user.save
+      		user_with_same_email.save
+      		 expect(user_with_same_email).not_to be_valid
+    	end
+  	end
 end
--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -4,19 +4,21 @@ RSpec.describe "users/index", :type => :view do
   before(:each) do
     assign(:users, [
       User.create!(
-        :name => "Name",
-        :email => "Email"
+      :name => "Example User",
+      :email => "example@email.com"
       ),
       User.create!(
-        :name => "Name",
-        :email => "Email"
+      :name => "Example User 2",
+      :email => "example_a@email.com"      
       )
     ])
   end
 
   it "renders a list of users" do
     render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Email".to_s, :count => 2
+    assert_select "tr>td", :text => "Example User".to_s, :count => 1
+    assert_select "tr>td", :text => "example@email.com".to_s, :count => 1
+    assert_select "tr>td", :text => "Example User 2".to_s, :count => 1
+    assert_select "tr>td", :text => "example_a@email.com".to_s, :count => 1
   end
 end
